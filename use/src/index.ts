7347d9f4bd434f5f2da44c8cd349e612fb9b9c6f
con <conmaster@noreply.com>
2026-01-22 20:58:50 +0100

    Init 0.3

--- use/src/index.ts
+++ use/src/index.ts
@@ -1,5 +1,5 @@
 import { CommandLine } from 'con-utils/cli';
-import { argv } from "node:process";
+import { argv } from 'node:process';
 const root = CommandLine.createGroup('.', 'Root');
 
 CommandLine.run(argv, root);
